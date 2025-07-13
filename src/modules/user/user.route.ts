8cdee9375cd1f38573768051286b6dc45a7bcbdc
Hera <mostafizhera@gmail.com>
2025-07-13 19:20:12 +0600

    create jwt and protect api

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,10 +1,15 @@
-import { Router } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
+import z from "zod";
+import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../../middlewares/validateRequest";
 
 
 const route = Router()
 
-route.post("/register", UserControllers.createUser)
+route.post("/register", validateRequest(createUserZodSchema), UserControllers.createUser)
+
 route.get("/all-users", UserControllers.getAllUsers)
 
 export const UserRoutes = route;
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,13 +1,29 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes"
+import bcrypt from "bcryptjs"
 
 
 
 const createUser = async (payload: Partial<IUser>) => {
-    const {name, email} = payload;
+    const {email, password, ...rest} = payload;
+
+    const isUserExist = await User.findOne({email})
+
+    if(isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User Alredy Exist")
+    }
+
+    const isBycriptPass = await bcrypt.hash(password as string, 10)
+
+    const authProvider: IAuthProvider = {provider: "credetials", providerId: email as string}
+
     const user = await User.create({
-        name,
-        email
+        email,
+        password: isBycriptPass,
+        auths: [authProvider],
+        ...rest
     })
     return user
 }
