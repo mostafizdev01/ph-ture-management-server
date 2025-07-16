522d58da8b7e61843f8d7b5b2f3d633ef6d59883
Hera <mostafizhera@gmail.com>
2025-07-17 02:41:54 +0600

    created successfully user forget password and change password

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import { JwtPayload } from "jsonwebtoken";
 import AppError from "../errorHelpers/AppError";
 import { verifyToken } from "../utilis/jwt";
 import { envVars } from "../config/env";
-import { Role } from "../modules/user/user.interface";
+import { isActive, Role } from "../modules/user/user.interface";
+import { User } from "../modules/user/user.model";
+import httpStatus from "http-status-codes"
 
 
-export const checkAuth = (...authRoles: string[]) => (req: Request, res: Response, next: NextFunction) => {
+export const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
     try {
         const accessToken = req.headers.authorization;
 
@@ -17,10 +19,25 @@ export const checkAuth = (...authRoles: string[]) => (req: Request, res: Respons
 
         const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_TOKEN) as JwtPayload
 
+        const isUserExist = await User.findOne({ email: verifiedToken.email })
+
+        if (!isUserExist) {
+            throw new AppError(httpStatus.BAD_REQUEST, "User does not exist")
+        }
+
+        if (isUserExist.isActive === isActive.BLOCKED || isUserExist.isActive === isActive.INACTIVE) {
+            throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
+        }
+
+        if (isUserExist.isDeleted) {
+            throw new AppError(httpStatus.BAD_REQUEST, `User is deleted`)
+        }
+
         if (!authRoles.includes(verifiedToken.role)) {
-            res.send({message: "You are not Permitted to view this route!!!"})
+            res.send({ message: "You are not Permitted to view this route!!!" })
             throw new AppError(403, "You are not Permitted to view this route!!!")
         }
+
         req.user = verifiedToken
         next()
 
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -9,12 +9,12 @@ import { setAuthCookie } from "../../utilis/setCookie";
 
 
 
-const credentialsLogin = catchAsync(async (req:Request, res:Response, next:NextFunction)=> {
+const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body)
 
     setAuthCookie(res, loginInfo);
 
-    sendResponse(res,{
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
         message: "User Logged In Successfully",
@@ -23,9 +23,9 @@ const credentialsLogin = catchAsync(async (req:Request, res:Response, next:NextF
 })
 
 // token controler>>>>>>>>>
-const getNewAccessToken = catchAsync(async (req:Request, res:Response, next:NextFunction)=> {
+const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken;
-    if(!refreshToken){
+    if (!refreshToken) {
         throw new AppError(httpStatus.BAD_REQUEST, "No refresh Token recieved from cookies")
     }
     const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string)
@@ -37,7 +37,7 @@ const getNewAccessToken = catchAsync(async (req:Request, res:Response, next:Next
 
     setAuthCookie(res, tokenInfo)
 
-    sendResponse(res,{
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
         message: "New Access Token Retrived Successfully ✅",
@@ -46,21 +46,21 @@ const getNewAccessToken = catchAsync(async (req:Request, res:Response, next:Next
 })
 
 // logout controler>>>>>>>>>
-const logout = catchAsync(async (req:Request, res:Response, next:NextFunction)=> {
-    
+const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
     res.clearCookie("accessToken", {
         httpOnly: true,
         secure: false,
         sameSite: "lax"
     })
-    
+
     res.clearCookie("refreshToken", {
         httpOnly: true,
         secure: false,
         sameSite: "lax"
     })
 
-    sendResponse(res,{
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
         message: "User logged Out Successfully",
@@ -68,8 +68,24 @@ const logout = catchAsync(async (req:Request, res:Response, next:NextFunction)=>
     })
 })
 
+// resetPassword controler>>>>>>>>>
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user;
+    await AuthServices.resetPassword(oldPassword, newPassword, decodedToken)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Password Changed Successfully",
+        data: null
+    })
+})
+
 export const AuthControllers = {
     credentialsLogin,
     getNewAccessToken,
-    logout
+    logout,
+    resetPassword
 }
